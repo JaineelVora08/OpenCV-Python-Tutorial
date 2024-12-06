e7d23d7a6766a50970c9819ade1f6cfb2e3ac416
JaineelVora08 <jaineelvora2006@gmail.com>
2024-12-06 18:09:26 +0530

    1_introduction.py

--- 1_introduction.py
+++ 1_introduction.py
@@ -2,11 +2,17 @@ import cv2
 
 img = cv2.imread("assets/cat.jpg", cv2.IMREAD_COLOR)
 print(img.shape) # print the image dimensions
+
 print(img[0, 0]) # print the first pixel
+
+#Note: jab grayscale image tab ek hi number return hoga btw 0 and 255 (0-black and 255-white)
+#      jab colour return hoga tab 3 numbers ka arrray return (RGB)
+#      imread returns a numpy array 
+
 # img = img * 2
 
 # Increase the values of every pixel
-# for i in range(img.shape[0]):
+# for i in range(img.shape[0]):          #shape[0] is no of rows and 1  is number of columns
 #     for j in range(img.shape[1]):
 #         img[i, j] = max(254, img[i, j] * 2)
 
